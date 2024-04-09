2da8eb3b16d3f0e47ee9dfb37b284d3a3e383ab6
kevin <kevinchevallier@gmail.com>
2024-04-09 15:20:14 -0300

    Add fetch for dolar value

---
+++ app/Content.tsx
@@ -0,0 +1,72 @@
+'use client'
+
+import { ChangeEvent, useState } from "react"
+
+const buyingWithCash = (ValorEmDolar: number, impostoDoEstado: number, valorDoDolar: number, IOFdaCompraDeDolar: number): number => {
+    const a = ValorEmDolar + impostoDoEstado
+    const b = valorDoDolar + IOFdaCompraDeDolar
+    return a * b
+}
+const buyingWithCard = (ValorEmDolar: number, impostoDoEstado: number, IOFdeTransaçõesInternacionais: number, valorDoDolar: number): number => {
+    const a = ValorEmDolar + impostoDoEstado + IOFdeTransaçõesInternacionais
+    return a * valorDoDolar
+}
+
+function Content(data: any) {
+    const [calculated, setCalculated] = useState(false)
+    const [result, setResult] = useState(0)
+    const [buyingOption, setBuyingOption] = useState('cash')
+    const [dolarValue, setDolarValue] = useState(1)
+    const [inputTax, setInputTax] = useState(1)
+
+    const valorDoDolar = data.data.USDBRL.high
+
+    return (
+        <div>
+            {!calculated ? (
+                <div>
+                    <div>
+                        <label htmlFor="inputDolar">Dolar</label>
+                        <input type="number" id="inputDolar" name="inputDolar" required value={dolarValue} onChange={(e: ChangeEvent<HTMLInputElement>) => setDolarValue(e.target.valueAsNumber)} />
+                    </div>
+
+                    <div>
+                        <label htmlFor="inputTax">Taxa do estado</label>
+                        <input type="number" id="inputTax" name="inputTax" required value={inputTax} onChange={(e: ChangeEvent<HTMLInputElement>) => setInputTax(e.target.valueAsNumber)} />
+                    </div>
+
+                    <fieldset>
+                        <legend>Tipo de compra</legend>
+
+                        <div>
+                            <input type="radio" id="cash" name="drone" value="cash" onClick={() => setBuyingOption('cash')} defaultChecked />
+                            <label htmlFor="cash">Dinhero</label>
+                        </div>
+                        <div>
+                            <input type="radio" id="card" name="drone" value="card" onClick={() => setBuyingOption('card')} />
+                            <label htmlFor="card">Cartao</label>
+                        </div>
+                    </fieldset>
+
+                    <button onClick={() => {
+                        setResult(buyingOption == 'cash' ? buyingWithCash(dolarValue, inputTax, valorDoDolar, 1) : buyingWithCard(dolarValue, inputTax, 5.3, valorDoDolar))
+                        setCalculated(true)
+                    }
+                    }>Converter</button>
+                </div>
+            ) : (
+                <div>
+                    <button onClick={() => setCalculated(false)}>Voltar</button>
+                    <footer style={{ marginTop: "50px" }}>
+                        <div>O resultado do calculo e: R$ {Math.round(result * 100) / 100}</div>
+                        <div>
+                            <p>Compra no dinehiro e taxa de {inputTax}%</p>
+                            <p>Cotacao do dolar: ${dolarValue} = R$ {Math.round(valorDoDolar * 100) / 100}</p>
+                        </div>
+                    </footer>
+
+                </div>)}
+        </div>)
+}
+
+export default Content
--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,18 @@
-'use client'
+import Content from "./Content"
 
-import { ChangeEvent, useState } from "react"
+async function getData() {
+  const res = await fetch('https://economia.awesomeapi.com.br/json/last/USD-BRL')
 
-const buyingWithCash = (ValorEmDolar: number, impostoDoEstado: number, valorDoDolar: number, IOFdaCompraDeDolar: number): number => {
-  const a = ValorEmDolar + impostoDoEstado
-  const b = valorDoDolar + IOFdaCompraDeDolar
-  return a * b
+  if (!res.ok) {
+    // This will activate the closest `error.js` Error Boundary
+    throw new Error('Failed to fetch data')
+  }
+
+  return res.json()
 }
-const buyingWithCard = (ValorEmDolar: number, impostoDoEstado: number, IOFdeTransaçõesInternacionais: number, valorDoDolar: number): number => {
-  const a = ValorEmDolar + impostoDoEstado + IOFdeTransaçõesInternacionais
-  return a * valorDoDolar
-}
 
-export default function Home() {
-  const [result, setResult] = useState(0)
-  const [buyingOption, setBuyingOption] = useState('cash')
-
-  const [dolarValue, setDolarValue] = useState(1)
-  const [inputTax, setInputTax] = useState(1)
+export default async function Home() {
+  const data = await getData()
 
   return (
     <main>
@@ -25,40 +20,7 @@ export default function Home() {
         <h1>Stone currency</h1>
       </header>
 
-      <div>
-        <div>
-          <label htmlFor="inputDolar">Dolar</label>
-          <input type="number" id="inputDolar" name="inputDolar" required value={dolarValue} onChange={(e: ChangeEvent<HTMLInputElement>) => setDolarValue(e.target.valueAsNumber)} />
-        </div>
-
-        <div>
-          <label htmlFor="inputTax">Taxa do estado</label>
-          <input type="number" id="inputTax" name="inputTax" required value={inputTax} onChange={(e: ChangeEvent<HTMLInputElement>) => setInputTax(e.target.valueAsNumber)} />
-        </div>
-
-        <fieldset>
-          <legend>Tipo de compra</legend>
-
-          <div>
-            <input type="radio" id="cash" name="drone" value="cash" onClick={() => setBuyingOption('cash')} defaultChecked />
-            <label htmlFor="cash">Dinhero</label>
-          </div>
-          <div>
-            <input type="radio" id="card" name="drone" value="card" onClick={() => setBuyingOption('card')} />
-            <label htmlFor="card">Cartao</label>
-          </div>
-        </fieldset>
-
-        <button onClick={() => setResult(buyingOption == 'cash' ? buyingWithCash(dolarValue, inputTax, 5, 1) : buyingWithCard(dolarValue, inputTax, 5.3, 5))}>Converter</button>
-
-        <footer style={{marginTop: "50px"}}>
-          <div>O resultado do calculo e: R$ {result}</div>
-          <div>
-            <p>Compra no dinehiro e taxa de {inputTax}%</p>
-            <p>Cotacao do dolar: ${dolarValue} = R$ {"TODO"}</p>
-          </div>
-        </footer>
-      </div>
+      <Content data={data} />
     </main>
   );
 }
